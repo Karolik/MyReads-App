18353e82ea855d54be6706dd2fffdb4d7fe243cd
Karolik <36989581+Karolik@users.noreply.github.com>
2018-07-11 00:36:42 +0200

    Update ListBooks.js

--- src/ListBooks.js
+++ src/ListBooks.js
@@ -1,18 +1,10 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom'
-import { Route } from 'react-router-dom'
-import PropTypes from 'prop-types'
-import escapeRegExp from 'escape-string-regexp'
+//import PropTypes from 'prop-types'
 import Bookshelf from './Bookshelf'
-import Search from './Search'
 import * as BooksAPI from './BooksAPI'
 
 class ListBooks extends Component {
-  static propTypes = {
-    books: PropTypes.array.isRequired,
-    onDeleteBook: PropTypes.func.isRequired
-  }
-  
   state = {
     books: [],
     shelf: ['read', 'currentlyReading', 'toRead'],
@@ -20,6 +12,8 @@ class ListBooks extends Component {
     currentlyReading: [],
     toRead: [],
     query: '',
+    //EMO:
+    //filteredBooks: filterBook("", 20),
     //For the select onChange function
     getInitialState: function() {
       return {
@@ -31,108 +25,132 @@ class ListBooks extends Component {
     }
   }
 
+  /*static propTypes = {
+    books: PropTypes.array.isRequired,
+    onDeleteBook: PropTypes.func.isRequired
+  }*/
+
+  //Fetch the data from the database BooksAPI.js
   componentDidMount() {
     BooksAPI.getAll().then((books) => {
       this.setState({ books })
     })
   }
 
-  // trim() - remove whitespace from both sides of a string
-  /*updateQuery = (query) => {
-    this.setState({ query: query.trim() })
+  //Update the shelf and books
+  /*componentDidMount() {
+    BooksAPI.update(book, shelf).then((books) => {
+      this.setState({ books })
+      .then((shelf) => {
+        this.setState({ shelf})
+      }
+    )
+    })
   }*/
-/*
-  clearQuery = () => {
-    this.setState({ query: '' })
-  }*/
+
+  //EMO:
+  filterBook(searchText, maxResults) { (book) => {
+    this.setState((state) => ({
+      books: state.books.filter(book => {
+        if (book.title.toLowerCase().includes(searchText.toLowerCase())) {
+          return true;
+        }
+        if (book.keywords.includes(searchText)) {
+          return true;
+        }
+        return false;
+      })
+      .slice(0, maxResults)
+    })
+    )
+  }
+}
+  //EMO:
+  /*handleSearchChange = event => {
+    this.setState({
+      filteredBook: filterBook(event.target.value, 20)
+    });
+  };*/
+
+/* Option "None" - Remove a book from the list */
   removeBook = (book) => {
     this.setState((state) => ({
       books: state.books.filter((b) => b.id !== book.id)
     }))
   }
 
-  addToRead(book) {
+  //Change/update book/shelf
+  changeShelf(book, shelf) {
+    BooksAPI.update(book, shelf).then(book => {
+      this.setState(state => ({
+        shelf: state.shelf.concat([ book])
+      }))
+  })
+}
+    
+  /*addToRead(book) {
     BooksAPI.update(book).then(book => {
       this.setState(state => ({
         read: state.read.concat([ book])
       }))
     })
+  }*/
+  
+  //Update the position of the book in the shelves
+  /*updateBook = (book) => {
+    this.setState((state)) => ({
+
+    })
+    componentDidMount(){
+    BooksAPI.update(book, shelf).then((book) => {
+      shelf: state.shelf.concat([ book ])
+    })
+    }
   }
+  */
 
   render() {
-    const { books } = this.props
+    //const { books } = this.props
     //const { onDeleteBook } = this.props
-    const { query } = this.state
-
-    //For search results,search page:
-    let showingBooks
-    //showingBooks = books
-    if (query) {
-    const match = new RegExp(escapeRegExp(query), 'i')
-     // const match = new RegExp((query), 'i')
-      //!! REMOVE It should first show the books - it filters but doesn't show any books before.
-    showingBooks = books.filter((book) => match.test(book.title))
-    } else {
-      showingBooks = books
-    }
-
-    //showingBooks.sort(sortBy('name')) 
 
     return (
-      <div className="app">
-        <Route path='/search' render={({ history }) => (
+      <div>
+        <div className="list-books-title">
+          <h1>MyReads</h1>
+        </div>
+        <div className="list-books-content">
           <div>
-            <Search
+            <div className="bookshelf">
+              <h2 className="bookshelf-title">Currently Reading</h2>
+              <Bookshelf    
+              onDeleteBook={this.removeBook}
+              books={this.state.books} />
+            </div>
+            <div className="bookshelf">
+              <h2 className="bookshelf-title">Want to Read</h2>
+              <Bookshelf    
+              onDeleteBook={this.removeBook}
+              books={this.state.books} />
+            </div>
+            <div className="bookshelf">
+              <h2 className="bookshelf-title">Read</h2>
+              <Bookshelf    
+              onDeleteBook={this.removeBook}
               books={this.state.books}
-              /*
-                onCreateContact={(contact) => {
-                this.createContact(contact)
-                history.push('/')
-              }}
-              */
-            />
-          </div>
-        )}/>
-        <Route exact path='/' render={() => (
-          <div className="list-books">
-            <div className="list-books-title">
-              <h1>MyReads</h1>
-            </div>
-            <div className="list-books-content">
-              <div>
-                <div className="bookshelf">
-                  <h2 className="bookshelf-title">Currently Reading</h2>
-                  <Bookshelf    
-                  onDeleteBook={this.removeBook}
-                  books={this.state.books} />
-                </div>
-                <div className="bookshelf">
-                  <h2 className="bookshelf-title">Want to Read</h2>
-                  <Bookshelf    
-                  onDeleteBook={this.removeBook}
-                  books={this.state.books} />
-                </div>
-                <div className="bookshelf">
-                  <h2 className="bookshelf-title">Read</h2>
-                  <Bookshelf    
-                  onDeleteBook={this.removeBook}
-                  books={this.state.books}
-                  read={this.state.read}
-                  onRead={(book) => {
-                    this.addToRead(book)
-                    //history.push('/')
-                  }} />
-                </div>
-              </div>
-            </div>
-            <div className="open-search">
-              <Link
-                to='/search'
-                //onClick={() => this.setState({ showSearchPage: true })}
-                >Add a book</Link>
+              read={this.state.read}
+              onRead={(book) => {
+                this.addToRead(book)
+                //history.push('/')
+              }} />
             </div>
           </div>
-        )}/>
+        </div>
+        <div className="open-search">
+          <Link
+            to='/search'
+            //onClick={() => this.setState({ showSearchPage: true })}
+            >Add a book</Link>
+        </div>
       </div>
     )
   }
