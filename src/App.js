721126607a163e049e668874d3fceee224739bc0
Karolik <36989581+Karolik@users.noreply.github.com>
2018-07-11 00:39:32 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,52 +1,39 @@
 import React, { Component } from 'react';
+import { Route } from 'react-router-dom'
 import ListBooks from './ListBooks'
-//import * as BooksAPI from './BooksAPI'
+import Search from './Search'
 import './App.css'
 
 class BooksApp extends Component {
-  state = {
-    books: [],
-    //screen: 'list',
-    shelves: ['read', 'currentlyReading', 'toRead'],
-    read: [],
-    currentlyReading: [],
-    toRead: [],
-    /**
-     * TODO: Instead of using this state variable to keep track of which page
-     * we're on, use the URL in the browser's address bar. This will ensure that
-     * users can use the browser's back and forward buttons to navigate between
-     * pages, as well as provide a good URL they can bookmark and share.
-     */
-    //showSearchPage: false
-  }
-
-  /* Option "None" - Remove a book from the list */
-  removeBook = (book) => {
-    this.setState((state) => ({
-      books: state.books.filter((b) => b.id !== book.id)
-    }))
-  }
-  /*
-    componentDidMount(){
-    BooksAPI.update(book, shelf).then((book) => {
-      shelf: state.shelf.concat([ book ])
-    })
-    }
-  }
-  */
-
   render() {
     return (
-      <div>
+      <div className="app">
         {//this.state.screen === "list"
         }
-        <ListBooks    
-        onDeleteBook={this.removeBook}
-        books={this.state.books}
-        read={this.state.read}
-        //shelf={this.state.shelf}
-        //onUpdate={() => {this.setState({shelf:'read'}) }} 
-        />
+        <Route path='/search' render={({ history }) => (
+          <div>
+            <Search
+              //books={this.state.books}
+              //textChange={this.handleSearchChange}
+              /*
+                onCreateContact={(contact) => {
+                this.createContact(contact)
+                history.push('/')
+              }} */
+            />
+          </div>
+        )}/>
+        <Route exact path='/' render={() => (
+          <div className="list-books">
+            <ListBooks    
+              //onDeleteBook={this.removeBook}
+              //books={this.state.books}
+              //read={this.state.read}
+              //shelf={this.state.shelf}
+              //onUpdate={() => {this.setState({shelf:'read'}) }} 
+              />
+          </div>
+        )}/>
       </div>
     )
   }
