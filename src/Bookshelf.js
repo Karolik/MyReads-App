e65850cd7ada931a71696c0183b7653f3de78ccc
Karolik <36989581+Karolik@users.noreply.github.com>
2018-07-11 00:38:45 +0200

    Update Bookshelf.js

--- src/Bookshelf.js
+++ src/Bookshelf.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types'
 import escapeRegExp from 'escape-string-regexp'
+import Book from './Book'
+import * as BooksAPI from './BooksAPI'
 
 class Bookshelf extends Component {
   static propTypes = {
@@ -8,6 +10,7 @@ class Bookshelf extends Component {
     //onDeleteBook: PropTypes.func.isRequired
   }
   state = {
+    books: [],
     getInitialState: function() {
       return {
           value: 'select'
@@ -23,6 +26,13 @@ class Bookshelf extends Component {
     }
   }
 
+  //Fetch the data from the database BooksAPI.js
+  componentDidMount() {
+    BooksAPI.getAll().then((books) => {
+      this.setState({ books })
+    })
+  }
+
   handleRead = (e) => {
     e.preventDefault()
     const values = e.target
@@ -33,41 +43,13 @@ class Bookshelf extends Component {
 
   render() {
     const { books, onDeleteBook } = this.props
-    const { query } = this.state
-    let showingBooks
-    if (query) {
-    const match = new RegExp(escapeRegExp(query), 'i')
-    //const match = new RegExp((query), 'i')
-    showingBooks = books.filter((book) => match.test(book.title, book.authors))
-    } else {
-    showingBooks = books
-    }
 
     return(
       <div className="bookshelf-books">
         <ol className="books-grid">
-        {showingBooks.map((book) => (
-          <li key={book.id}>
-            <div className="book">
-              <div className="book-top">
-                <div className="book-cover" style={{ width: 128, height: 193,
-                    backgroundImage: `url(${book.imageLinks.thumbnail})`
-                    }}></div>
-                <div className="book-shelf-changer">
-                  <select /*change it*/ onChange={this.handleRead} value={this.state.value} /*onChange={() => ((this.value === "none") ? onDeleteBook(book) : null)}*/>
-                    <option value="move" disabled>Move to...</option>
-                    <option value="currentlyReading">Currently Reading</option>
-                    <option value="wantToRead">Want to Read</option>
-                    <option value="read">Read</option>
-                    <option className="none" value="none" /*onClick={() => onDeleteBook(book)}*/>None</option>
-                  </select>
-                </div>
-              </div>
-              <div className="book-title">{book.title}</div>
-              <div className="book-authors">{book.authors}</div>
-            </div>
-          </li>
-        ))}
+          <Book  
+            onDeleteBook={this.removeBook}
+            books={this.state.books} />
         </ol>
       </div>
     )
