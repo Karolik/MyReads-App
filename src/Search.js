8016dd94aa181b30dfcc09b19a7327b6c5da8d2a
Karolik <36989581+Karolik@users.noreply.github.com>
2018-07-12 00:51:38 +0200

    Update Search.js

--- src/Search.js
+++ src/Search.js
@@ -1,9 +1,6 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom'
 import Bookshelf from './Bookshelf'
-import Book from './Book'
-import PropTypes from 'prop-types'
-import escapeRegExp from 'escape-string-regexp'
 import * as BooksAPI from './BooksAPI'
 
 class Search extends Component {
@@ -11,11 +8,6 @@ class Search extends Component {
         books: [],
         query: ''
       }
-    //If I put that, I need to have state defined in App.js
-    /*static propTypes = {
-    books: PropTypes.array.isRequired,
-    //onDeleteBook: PropTypes.func.isRequired
-    } */
 
     //Fetch the data from the database BooksAPI.js
     componentDidMount() {
@@ -28,34 +20,18 @@ class Search extends Component {
     updateQuery = (query) => {
         this.setState({ query: query.trim() })
     }
-    /*
+    
     clearQuery = () => {
         this.setState({ query: '' })
-    }*/
-
-    /*Search books
-    componentDidMount() {
-        BooksAPI.search().then((books) => {
-            this.setState({ books })
-          })
-    } */
+    }
     
     render() {
-        const { books, onDeleteBook } = this.props
         const { query } = this.state
-
-        let showingBooks
-        if (query) {
-            const match = new RegExp(escapeRegExp(query), 'i')
-            showingBooks = books.filter((book) => match.test(book.title, book.authors))
-        } else {
-            showingBooks = books
-        }
 
         return (
         <div className="search-books">
             <div className="search-books-bar">
-            <Link to='/' className="close-search" /*onClick={() => this.setState({ showSearchPage: false })}*/>Close</Link>
+            <Link to='/' className="close-search">Close</Link>
                 <div className="search-books-input-wrapper">
                 {/*
                     NOTES: The search from BooksAPI is limited to a particular set of search terms.
