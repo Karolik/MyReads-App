98efe9a2b13f30b5221872c94c0278b5be7403da
Karolik <36989581+Karolik@users.noreply.github.com>
2018-07-15 23:32:56 +0200

    Update Bookshelf.js

--- src/Bookshelf.js
+++ src/Bookshelf.js
@@ -1,25 +1,23 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Book from './Book'
 
-class Bookshelf extends Component {
-  render() {
-
-    return(
-      <div className="bookshelf-books">
-        <ol className="books-grid">
-        {this.props.books.map((book, index) =>
-          <Book  
-            key={index}
-            book={book}
-            onChangeShelf={this.changeShelf}
-            onDeleteBook={this.removeBook}
-            //books={this.props.books} 
-            />
-          )}
-        </ol>
-      </div>
-    )
-  }
+const Bookshelf = (props) => {
+  return(
+    <div className="bookshelf-books">
+      <ol className="books-grid">
+      {props.books.map((book, index) =>
+        <Book  
+          key={index}
+          book={book}
+          //value={this.value}
+          onChangeShelf={this.changeShelf}
+          onDeleteBook={this.removeBook}
+          //books={this.props.books} 
+          />
+        )}
+      </ol>
+    </div>
+  )
 }
 
 export default Bookshelf;
