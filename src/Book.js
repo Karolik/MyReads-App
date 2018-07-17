a951a781574044ac11dd76236e604684992d5e5b
Karolik <36989581+Karolik@users.noreply.github.com>
2018-07-18 01:01:18 +0200

    Update Book.js

--- src/Book.js
+++ src/Book.js
@@ -7,7 +7,7 @@ const Book = (props) => {
           <div className="book">
             <div className="book-top">
               <div className="book-cover" style={{ width: 128, height: 193,
-                backgroundImage: `url(${props.book.imageLinks.thumbnail})`
+                //backgroundImage: `url(${props.book.imageLinks.thumbnail})`
               }}></div>
               <div className="book-shelf-changer">
                 <select
