98c67f896c9ab3beae2714b5a6e919bc029d912b
Karolik <36989581+Karolik@users.noreply.github.com>
2018-07-19 12:59:01 +0200

    Update Book.js

--- src/Book.js
+++ src/Book.js
@@ -8,8 +8,7 @@ const Book = (props) => {
         <li key={props.book.id}>
           <div className="book">
             <div className="book-top">
-              <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${bookImage})`
-              }}></div>
+              <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${bookImage})`}}></div>
               <div className="book-shelf-changer">
                 <select
                   onChange={event => props.changeShelf(props.book, event.target.value)}
