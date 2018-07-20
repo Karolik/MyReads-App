2f2ebffd6ecdc55ef56bfb18c426a4220f6f4cc5
Karolik <36989581+Karolik@users.noreply.github.com>
2018-07-20 20:02:33 +0200

    Update Book.js

--- src/Book.js
+++ src/Book.js
@@ -23,7 +23,7 @@ const Book = (props) => {
               </div>
             </div>
             <div className="book-title">{props.book.title}</div>
-            <div className="book-authors">{props.book.authors}</div>
+            <div className="book-authors">{props.book.authors ? props.book.authors.toString() : ''}</div>
           </div>
         </li>
     </div>
